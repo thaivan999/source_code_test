1a4f3de9849c10d37fbfa1d949371daee8fbd8d7
ThaiVanHandSome <danitbadao1234@gmail.com>
2024-11-27 13:57:29 +0700

    init

---
+++ app.js
@@ -0,0 +1,55 @@
+
+        var swiperz247b5f63z2a32z4f00zb717z718c8b064aa4 = new Swiper(".swiper-247b5f63-2a32-4f00-b717-718c8b064aa4", {
+          effect: "fade",
+          slidesPerView: 1,
+          loop: true,
+          grid: {
+            rows: 1,
+            fill: 'row'
+          },
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+          },
+          scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true
+          },
+          
+          autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+          },
+        });
+      
+        var swiperzea4be962z2a4bz4b2fz8bfez3ade60dcd7ad = new Swiper(".swiper-ea4be962-2a4b-4b2f-8bfe-3ade60dcd7ad", {
+          effect: "slide",
+          slidesPerView: 2,
+          loop: true,
+          grid: {
+            rows: 1,
+            fill: 'row'
+          },
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+          },
+          scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true
+          },
+          
+          autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+          },
+        });
+      
