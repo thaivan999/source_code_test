bfe3f72722ee4ca6299dc5ce4bb782e67fb4b8db
ThaiVanHandSome <danitbadao1234@gmail.com>
2024-12-19 12:32:43 +0700

    new

---
+++ assets/app.js
@@ -0,0 +1,19 @@
+
+
+const header = document.querySelector('header');
+let oldScroll = 0;
+window.addEventListener('scroll', () => {
+  const scrollTop = window.scrollY; 
+  const viewportHeight = window.innerHeight; 
+  if (scrollTop > viewportHeight) { 
+    if (oldScroll > scrollTop) {
+      header.classList.add('active'); 
+    } else {
+      header.classList.remove('active');
+    }
+  } else {
+    header.classList.remove('active');
+  }
+  oldScroll = scrollTop; 
+});
+    
